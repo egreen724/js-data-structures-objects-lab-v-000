60bc94647236d6f4a7af9f50cb212b32b4ad863f
Erin Green <erin.y.green@gmail.com>
2019-05-08 19:30:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+
+driver = {}
+
+function updateDriverWithKeyAndValue(){
+  
+}
+
+function destructivelyUpdateDriverWithKeyAndValue() {
+  
+}
+
+function deleteFromDriverByKey() {
+  
+}
+
+function destructivelyDeleteFromDriverByKey() {
+  
+}
+
